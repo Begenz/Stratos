cc4a5cd45be56cd910ab6c75b46b3edb58fa9c80
Begenz <dlbozz2020@gmail.com>
2025-09-14 22:58:07 -0400

    Create global.js

---
+++ messages/rpg/global.js
@@ -0,0 +1,11 @@
+// messages/rpg/state.js
+
+const playerInventory = {};
+const playerStats = {};
+const activeBattles = {};
+
+module.exports = {
+  playerInventory,
+  playerStats,
+  activeBattles,
+};
